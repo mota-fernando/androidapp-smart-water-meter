ef8358997f92a0848f34e8357079a85295512167
mota-fernando <motafn+github@gmail.com>
2018-11-15 01:56:01 -0300

    Criação da TAB preferências para controle de dois swtiches, fluxodeagua e inteligenciaartificial.

---
+++ app/src/main/java/mota/fernando/hidrometrointeligente/Config.java
@@ -0,0 +1,25 @@
+package mota.fernando.hidrometrointeligente;
+
+public class Config {
+
+    private int fluxoagua;
+    private int inteligenciaartificial;
+
+    public Config() {}
+
+    public int getFluxoagua() {
+        return fluxoagua;
+    }
+
+    public void setFluxoagua(int fluxoagua) {
+        this.fluxoagua = fluxoagua;
+    }
+
+    public int getInteligenciaartificial() {
+        return inteligenciaartificial;
+    }
+
+    public void setInteligenciaartificial(int inteligenciaartificial) {
+        this.inteligenciaartificial = inteligenciaartificial;
+    }
+}
--- app/src/main/java/mota/fernando/hidrometrointeligente/Preferencias.java
+++ app/src/main/java/mota/fernando/hidrometrointeligente/Preferencias.java
@@ -1,25 +1,80 @@
 package mota.fernando.hidrometrointeligente;
 
-public class Preferencias {
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Switch;
 
-    private int fluxoAgua;
-    private int inteligenciaArtificial;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-    public Preferencias() {}
+public class Preferencias extends Fragment {
 
-    public int getFluxoAgua() {
-        return fluxoAgua;
+
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference preferencias = databaseReference.child("Preferencias");
+    Config config;
+    Switch switchfa, switchia;
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.activity_preferencias, container, false);
+
+        switchfa = (Switch) rootView.findViewById(R.id.switchfa);
+        switchia = (Switch) rootView.findViewById(R.id.switchia);
+
+        config = new Config();
+
+
+        Button btnConfirmaSwitch = (Button)rootView.findViewById(R.id.btnConfirmaSwitch);
+        btnConfirmaSwitch.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+                if(switchfa.isChecked())
+                    config.setFluxoagua(1);
+                else
+                    config.setFluxoagua(0);
+                if(switchia.isChecked())
+                    config.setInteligenciaartificial(1);
+                else
+                    config.setInteligenciaartificial(0);
+                preferencias.setValue(config);
+
+            }
+        });
+
+
+       preferencias.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                config = dataSnapshot.getValue(Config.class);
+                if (config.getFluxoagua()==1)
+                    switchfa.setChecked(true);
+                else
+                    switchfa.setChecked(false);
+
+                if (config.getInteligenciaartificial()==1)
+                    switchia.setChecked(true);
+                else
+                    switchia.setChecked(false);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        return rootView;
     }
 
-    public void setFluxoAgua(int fluxoAgua) {
-        this.fluxoAgua = fluxoAgua;
-    }
-
-    public int getInteligenciaArtificial() {
-        return inteligenciaArtificial;
-    }
-
-    public void setInteligenciaArtificila(int inteligenciaArtificila) {
-        this.inteligenciaArtificial = inteligenciaArtificila;
-    }
 }
