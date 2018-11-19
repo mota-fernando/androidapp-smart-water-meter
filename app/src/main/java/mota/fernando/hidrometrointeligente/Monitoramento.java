b3af770798fef4c6b5ed1732b54609922a06064b
mota-fernando <motafn+github@gmail.com>
2018-11-19 18:53:25 -0300

    Criação da TAB Monitoramento: adição de Gauge Graph em tempo real.

--- app/src/main/java/mota/fernando/hidrometrointeligente/Monitoramento.java
+++ app/src/main/java/mota/fernando/hidrometrointeligente/Monitoramento.java
@@ -1,20 +1,91 @@
 package mota.fernando.hidrometrointeligente;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.anastr.speedviewlib.SpeedView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import static android.view.View.*;
+
+
 public class Monitoramento extends Fragment {
+
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+
+    Query consumoquery = databaseReference.child("Consumo").orderByChild("xvalue").limitToLast(1);
+    Handler mHandler;
+    SpeedView speedView;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_monitoramento, container, false);
-        /*
 
-        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-        textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));*/
+        this.mHandler = new Handler();
+        m_Runnable.run();
+
+        speedView = (SpeedView) rootView.findViewById(R.id.speedView);
+        speedView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(speedView.getUnit() == "m³") {
+                    speedView.speedTo(speedView.getSpeed());
+                    speedView.setUnit("l/m");
+                }else{
+                    speedView.speedTo(speedView.getSpeed());
+                    speedView.setUnit("m³");
+                }
+            }
+        });
         return rootView;
     }
+
+    public void rMonitor() {
+
+        consumoquery.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot child : dataSnapshot.getChildren())
+                {
+
+                        Consumo consumo = child.getValue(Consumo.class);
+                        drawtext(consumo.getyvalue());
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getMessage());
+            }
+
+        });
+    }
+    public void drawtext (int v) {
+
+
+        speedView.setMaxSpeed(320);
+
+        speedView.speedTo(v);
+
+    }
+
+    private final Runnable m_Runnable = new Runnable() {
+        public void run()
+
+        {
+            rMonitor();
+            mHandler.postDelayed(m_Runnable, 5000);
+        }
+    };
 }
